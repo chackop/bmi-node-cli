f5bbd1bb1ff7b040714aeb15c2cc55009a51431d
Chacs <chacko@skykatltd.com>
2022-10-15 16:15:00 +0100

    cleaned lib

--- build/utils/calculateBMI/calculateBMI.spec.js
+++ build/utils/calculateBMI/calculateBMI.spec.js
@@ -5,11 +5,12 @@ const _1 = tslib_1.__importDefault(require("."));
 describe("calculateBMI", () => {
     it.each `
     heightInCM | weightInKilos | calcBMIResult
-    ${171}     | ${96}         | ${1.78}
-    ${161}     | ${85}         | ${1.89}
-    ${180}     | ${77}         | ${0.23}
-    ${150}     | ${62}         | ${0.24}
-    ${167}     | ${82}         | ${41}
+    ${171}     | ${96}         | ${32}
+    ${161}     | ${85}         | ${28}
+    ${180}     | ${77}         | ${26}
+    ${166}     | ${62}         | ${21}
+    ${150}     | ${70}         | ${23}
+    ${167}     | ${82}         | ${27}
   `("calculates BMI value: $calcBMIResult based on supplied height: $heightInCM and weight: $weightInKilos ", ({ heightInCM, weightInKilos, calcBMIResult }) => {
         expect((0, _1.default)(heightInCM, weightInKilos)).toStrictEqual(calcBMIResult);
     });
